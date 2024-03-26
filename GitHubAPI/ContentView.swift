f85eabb6585891606103f57e5991f9adf17a68b7
marti <martiespinosa99@gmail.com>
2024-03-26 21:46:57 +0100

    Initial Commit

---
+++ GitHubAPI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitHubAPI
+//
+//  Created by Martí Espinosa Farran on 26/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitHubAPI/GitHubAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubAPIApp.swift
+//  GitHubAPI
+//
+//  Created by Martí Espinosa Farran on 26/3/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHubAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
