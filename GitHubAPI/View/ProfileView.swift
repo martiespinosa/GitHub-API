f176760841062db9fb0e396c25c74afa7241a7d7
marti <martiespinosa99@gmail.com>
2024-04-14 16:47:35 +0200

    no picker

--- GitHubAPI/View/ProfileView.swift
+++ GitHubAPI/View/ProfileView.swift
@@ -12,54 +12,49 @@ struct ProfileView: View {
     // MARK: - PROPERTIES
     
     @State var user: GHUser
-    @State private var pickerSelection: Int = 1
     
     @State private var reposList: [Repo] = []
     @State private var followersList: [GHUser] = []
     @State private var followingList: [GHUser] = []
     
+    @State private var searchTerm: String = ""
+    var filteredFollowers: [GHUser] {
+        guard !searchTerm.isEmpty else { return followersList }
+        return followersList.filter { $0.login.localizedCaseInsensitiveContains(searchTerm) }
+    }
+    var filteredFollowing: [GHUser] {
+        guard !searchTerm.isEmpty else { return followingList }
+        return followingList.filter { $0.login.localizedCaseInsensitiveContains(searchTerm) }
+    }
+    
     // MARK: - BODY
     
     var body: some View {
         NavigationStack {
-            //ScrollView {
+            ScrollView {
                 VStack(alignment: .leading, spacing: 25) {
                     userMainInfo
                     userBio
-                    
-                    segmentedPicker
-                    switch pickerSelection {
-                    case 1:
-                        repos
-                            .padding(-16)
-                    case 2:
-                        followers
-                            .padding(-16)
-                    case 3:
-                        following
-                            .padding(-16)
-                    default:
-                        EmptyView()
-                    }
-                    Spacer()
+                    userFollow
+                    userRepos
                 }
                 .padding(16)
                 .navigationTitle("Profile")
-            //}
             }
-            .task {
-                do {
-                    user = try await getUser(login: user.login)
-                } catch GHError.invalidURL {
-                    print("Error: Invalid URL.")
-                } catch GHError.invalidResponse {
-                    print("Error: Invalid Response.")
-                } catch GHError.invalidData {
-                    print("Error: Invalid Data.")
-                } catch {
-                    print("Unknown Error.")
-                }
+        }
+        .task {
+            do {
+                user = try await getUser(login: user.login)
+            } catch GHError.invalidURL {
+                print("Error: Invalid URL.")
+            } catch GHError.invalidResponse {
+                print("Error: Invalid Response.")
+            } catch GHError.invalidData {
+                print("Error: Invalid Data.")
+            } catch {
+                print("Unknown Error.")
             }
+        }
     }
     
     // MARK: - FUNCTIONS
@@ -71,6 +66,21 @@ struct ProfileView: View {
             return String(number / 1000000) + "M"
         } else {
             return String(number)
+        }
+    }
+    
+    func colorByLanguge(language: String) -> Color {
+        switch language {
+        case "Python", "C", "C#", "C++", "CSS":
+                .blue
+        case "Kotlin":
+                .purple
+        case "Java", "HTML", "Swift":
+                .orange
+        case "JavaScript":
+                .yellow
+        default:
+                .secondary
         }
     }
 }
@@ -114,44 +124,68 @@ extension ProfileView {
             .foregroundStyle(.secondary)
     }
     
-    private var segmentedPicker: some View {
-        Picker(
-            selection: $pickerSelection,
-            label: Text("Segmented picker"),
-            content: {
-                Text("\(truncateNumberIfNeeded(number: user.publicRepos ?? 0)) Repos")
-                    .tag(1)
-                Text("\(truncateNumberIfNeeded(number: user.followers ?? 0)) Followers")
-                    .tag(2)
-                Text("\(truncateNumberIfNeeded(number: user.following ?? 0)) Following")
-                    .tag(3)
+    private var userFollow: some View {
+        HStack {
+            Image(systemName: "person.2")
+            NavigationLink(destination: followers) {
+                HStack {
+                    Text("\(truncateNumberIfNeeded(number: user.followers ?? 0))")
+                        .font(.headline)
+                    Text("Followers")
+                        .foregroundStyle(.secondary)
+                }
             }
-        )
-        .pickerStyle(SegmentedPickerStyle())
+            Text("·")
+            NavigationLink(destination: following) {
+                Text("\(truncateNumberIfNeeded(number: user.following ?? 0))")
+                    .font(.headline)
+                Text("Following")
+                    .foregroundStyle(.secondary)
+            }
+        }
+        .buttonStyle(PlainButtonStyle())
     }
-    
-    private var repos: some View {
-        List {
-//            if reposList.isEmpty {
-//                Text("Loading...")
-//            } else {
-                ForEach(reposList, id: \.self) { repo in
-                    NavigationLink(destination: RepoView(repo: repo)) {
-                        HStack {
-                            VStack(alignment: .leading) {
+
+    private var userRepos: some View {
+        VStack {
+            ForEach(reposList, id: \.self) { repo in
+                NavigationLink(destination: RepoView(repo: repo)) {
+                    HStack {
+                        VStack(alignment: .leading, spacing: 10) {
+                            HStack {
                                 Text(repo.name)
                                     .font(.title3)
                                     .fontWeight(.semibold)
-                                
-                                Text(repo.description ?? "")
+                                Spacer()
+                                Text("Public")
+                            }
+                            
+                            Text(repo.description ?? "")
+                                .foregroundStyle(.secondary)
+                            
+                            HStack {
+                                Circle()
+                                    .fill(colorByLanguge(language: "\(repo.language ?? "")"))
+                                    .frame(width: 15)
+
+                                Text(repo.language ?? "")
                                     .foregroundStyle(.secondary)
                             }
-                            Spacer()
+                            .padding(.top)
                         }
-                        .frame(maxWidth: .infinity)
+                        Spacer()
                     }
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(.primary.opacity(0.1))
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(.secondary, lineWidth: 2.5)
+                    )
                 }
-//            }
+                .buttonStyle(PlainButtonStyle())
+            }
         }
         .onAppear {
             getRepos(for: user.login, page: 1) { repos, errorMessage in
@@ -166,29 +200,36 @@ extension ProfileView {
     
     private var followers: some View {
         List {
-            ForEach(followersList, id: \.self) { follower in
-                NavigationLink(destination: ProfileView(user: follower)) {
-                    HStack {
-                        AsyncImage(
-                            url: URL(string: follower.avatarUrl)) { image in
-                                image
-                                    .resizable()
-                                    .scaledToFit()
-                                    .clipShape(Circle())
-                            } placeholder: {
-                                Circle()
-                                    .foregroundStyle(.secondary)
-                            }
-                            .frame(width: 50, height: 50)
-                        
-                        Text(follower.login)
-                            .font(.headline)
-                        Spacer()
+//            if filteredFollowers.isEmpty {
+//                ContentUnavailableView.search(text: searchTerm)
+//            } else {
+                ForEach(filteredFollowers, id: \.self) { follower in
+                    NavigationLink(destination: ProfileView(user: follower)) {
+                        HStack {
+                            AsyncImage(
+                                url: URL(string: follower.avatarUrl)) { image in
+                                    image
+                                        .resizable()
+                                        .scaledToFit()
+                                        .clipShape(Circle())
+                                } placeholder: {
+                                    Circle()
+                                        .foregroundStyle(.secondary)
+                                }
+                                .frame(width: 50, height: 50)
+                            
+                            Text(follower.login)
+                                .font(.headline)
+                            Spacer()
+                        }
+                        .frame(maxWidth: .infinity)
                     }
-                    .frame(maxWidth: .infinity)
+                    .navigationTitle("\(user.login) Followers")
                 }
-            }
+//            }
         }
+        .searchable(text: $searchTerm, prompt: "Search User")
+        .autocorrectionDisabled()
         .onAppear {
             getFollowers(for: user.login, page: 1) { followers, errorMessage in
                 guard let followers = followers else {
@@ -202,29 +243,36 @@ extension ProfileView {
     
     private var following: some View {
         List {
-            ForEach(followingList, id: \.self) { follower in
-                NavigationLink(destination: ProfileView(user: follower)) {
-                    HStack {
-                        AsyncImage(
-                            url: URL(string: follower.avatarUrl)) { image in
-                                image
-                                    .resizable()
-                                    .scaledToFit()
-                                    .clipShape(Circle())
-                            } placeholder: {
-                                Circle()
-                                    .foregroundStyle(.secondary)
-                            }
-                            .frame(width: 50, height: 50)
-                        
-                        Text(follower.login)
-                            .font(.headline)
-                        Spacer()
+//            if filteredFollowers.isEmpty {
+//                ContentUnavailableView.search(text: searchTerm)
+//            } else {
+                ForEach(filteredFollowing, id: \.self) { follower in
+                    NavigationLink(destination: ProfileView(user: follower)) {
+                        HStack {
+                            AsyncImage(
+                                url: URL(string: follower.avatarUrl)) { image in
+                                    image
+                                        .resizable()
+                                        .scaledToFit()
+                                        .clipShape(Circle())
+                                } placeholder: {
+                                    Circle()
+                                        .foregroundStyle(.secondary)
+                                }
+                                .frame(width: 50, height: 50)
+                            
+                            Text(follower.login)
+                                .font(.headline)
+                            Spacer()
+                        }
+                        .frame(maxWidth: .infinity)
                     }
-                    .frame(maxWidth: .infinity)
+                    .navigationTitle("\(user.login) Following")
                 }
-            }
+//            }
         }
+        .searchable(text: $searchTerm, prompt: "Search User")
+        .autocorrectionDisabled()
         .onAppear {
             getFollowing(for: user.login, page: 1) { followers, errorMessage in
                 guard let followers = followers else {
